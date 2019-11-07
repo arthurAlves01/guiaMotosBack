dc008b47c9e19cb6725c76452ff7476162066a52
rolivsan <rolivsan@gmail.com>
2019-11-06 21:54:06 -0300

    Adicionado algumas rotas

--- motos/urls.py
+++ motos/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$','motos.views.index'),
-    url(r'motos$', 'motos.view.exibir')
+    url(r'^$','motos.views.index', name='home'),
+    url(r'^motos$', 'motos.views.exibir', name='exibir'),
+    url(r'^login$', 'motos.views.login', name='login'),
+    url(r'^cadastra$', 'motos.views.cadastrar', name='cadastrar')
 )
--- motos/views.py
+++ motos/views.py
@@ -7,3 +7,9 @@ def index(request):
     
 def exibir(request):
     return render(request, 'exibir.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def cadastrar(request):
+    return render(request, 'cadastrar.html')
